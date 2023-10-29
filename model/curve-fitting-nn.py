f7cc12ad0509787f1207b8d623668f97069b4d1c
davmacario <dav.macario@gmail.com>
2023-10-28 21:52:51 -0500

    Update model code

--- model/curve-fitting-nn.py
+++ model/curve-fitting-nn.py
@@ -3,6 +3,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import os
 from typing import Callable
+import warnings
 
 
 def tanh_prime(x):
@@ -25,38 +26,270 @@ def mse(d: np.ndarray, y: np.ndarray) -> float:
     return (1 / n) * np.sum((d - y) ** 2)
 
 
-def nn_output(x: float, w: np.ndarray, N: int, phi: Callable) -> [float, np.ndarray]:
+def targetFunction(x: float) -> float:
     """
-    Evaluate the output of the neural network
+    targetFunction
+    ---
+    Sample function to be approximated by the model.
 
-    ### Input parameters
-    - x: input of NN (single value, in this case)
-    - w: weights of NN (3N+1 elements)
-    - N: number of perceptron in central layer
-    - phi: activation function in central layer (output is linear)
-
-    ### Output values
-    - y: output of NN
-    - v: intermediate values at central layer (before activation)
+    $ y = \sin{(20x)} + 3x$
     """
-    assert (
-        w.shape[0] == 3 * N + 1
-    ), f"The array of weights has the wrong size; it should be {3 * N + 1} x 1"
+    return np.sin(20 * x) + 3 * x
 
-    # Isolate elements in array w
-    b_i = w[:N]  # Biases of 1st layer
-    w_ij = w[N : 2 * N]  # Weights of 1st layer
-    w_prime_ij = w[2 * N : 3 * N]  # Weights of 2nd layer (to output)
-    b_prime = w[-1]  # Weight of output
 
-    # Evaluate intermediate values:
-    v = x * w_ij + b_i
-    # Pass them through activation function:
-    z = phi(v)
-    # Evaluate output (y)
-    y = sum(z * w_prime_ij) + b_prime
+class CurveFitter:
+    """
+    CurveFitter
+    ---
 
-    return y, v
+    """
+
+    def __init__(
+        self,
+        n_neurons_hidden: int,
+        target_function: Callable = targetFunction,
+        act_func: Callable = np.tanh,
+        act_func_deriv: Callable = tanh_prime,
+    ):
+        """
+        Initialize CurveFitter object.
+
+        The implemented neural network is single-input, single-output, with one hidden layer.
+
+        ### Input parameters
+        - n_neurons_hidden: number of neurons in the hidden layer.
+        """
+
+        self.n_hidden = n_neurons_hidden
+        self.n_params = 3 * self.n_hidden + 1  # Number of network parameters
+
+        # Initialized weights:
+        self.w = np.random.normal(0, 1, (self.n_params))
+        # Activation function:
+        self.phi = act_func
+
+        # Target function - to be approximated
+        self.target = target_function
+
+        self._train_init = False  # True if the training set was initialized
+
+        # ----- Placeholders -----
+        # Training elements (and noise) need to be created with the
+        # `createTrainSet` method
+        self.n_train = None
+        self.x_train = None
+        self._noise = None
+        self.y_train = None
+        self.batch_size = 0  # If set to 1, the update is 'online'
+
+        self.eta = None
+
+    def createTrainSet(self, n_train: int) -> [np.ndarray, np.ndarray]:
+        """
+        createTrainSet
+        ---
+        Initialize the training set.
+
+        The values of x are uniformly-distributed in [0, 1], while the values of
+        y are given by self.target, plus the addition of uniform random noise in
+        [-0.1, 0.1].
+
+        The function updates the class attributes, but it also returns the
+        generated x_train and y_train vectors.
+
+        ### Input parameters
+        - n_train: number of training elements.
+
+        ### Output parameters:
+        - self.x_train
+        - self.y_train
+        """
+        if self.target is None:
+            raise ValueError(
+                "No target function was provided - unable to generate training set!"
+            )
+
+        self.n_train = n_train
+        self.x_train = np.random.uniform(0, 1, (self.n_train, 1))
+        self._noise = np.random.uniform(-0.1, 0.1, (self.n_train, 1))
+
+        self.y_train = self.target(self.x_train) + self.noise
+        self._train_init = True
+
+        return self.x_train, self.y_train
+
+    def assignTrainSet(self, x_train, y_train):
+        """
+        assignTrainSet
+        ---
+        Pass training values to the model.
+
+        This method should be used when the target function is not known
+        and only the training points are given.
+
+        Note that this method will overwrite the current training values.
+
+        ### Input parameters
+        - x_train
+        - y_train
+        """
+        if self._train_init:
+            warnings.warn("Overwriting existing training set!")
+
+        self.x_train = x_train
+        self.y_train = y_train
+        self._train_init = True
+
+    def checkTrainInit(self) -> int:
+        """Check whether the training set is assigned"""
+        if not self._train_init:
+            raise ValueError("The training set is not defined!")
+        else:
+            return 1
+
+    def setLearningRate(self, learn_rate: float):
+        """
+        setLearningRate
+        ---
+        Assign the learning rate.
+        """
+        if learn_rate > 0:
+            self.eta = learn_rate
+        else:
+            raise ValueError("The learning rate must be strictly positive")
+
+    def train(self, n_epochs: int, batch_size: int = 1, norm: bool = False):
+        """
+        train
+        ---
+        Launch the training algorithm, via backpropagation.
+
+        The only stopping condition is given by the number of epochs.
+
+        ### Input parameters
+        - n_epochs: number of epochs
+        - batch_size: default = 1
+        - norm: if true, normalize inputs
+
+        ### Output parameters
+        - grad_matrix: (n_parameters x n_updates) matrix having as columns the gradient
+        values at each update; the number of updates is: floor(n_epochs * n_train / batch_size)
+        """
+        # TODO: checks
+        self.checkTrainInit()
+
+        if self.batch_size != 0 and batch_size != self.batch_size:
+            warnings.warn("The batch size changed!")
+
+        self.batch_size = batch_size
+
+        y_curr = np.zeros((self.n_train, 1))
+        v_curr = np.zeros(
+            (self.n_train, self.n_hidden)
+        )  # Intermediate values (for backpropagation)
+
+        # Number of iterations per epoch is given by the batch size
+        n_iter_epoch = np.ceil(self.n_train / batch_size)
+
+        for i in range(self.n_train):
+            y_curr[i], v = self.forward(self.x_train[i])
+            v_curr[i, :] = v.T
+
+        mse_curr = mse(self.y_train, y_curr)  # Epoch 0 MSE
+        curr_grad = np.zeros(self.w.shape)
+
+        for i in range(n_iter_epoch):
+            grad_sum = np.zeros(self.w.shape)
+            self.mse_per_epoch.append(mse_curr)
+            for j in range(batch_size):
+                # For each batch, evaluate the gradients, then average and update w
+                curr_index = i * batch_size + j
+                y, v = self.forward(self.x_train[curr_index])
+                y_curr[curr_index] = y
+                grad_sum = self.grad(
+                    self.x_train[curr_index], self.y_train[curr_index], y, v
+                )
+
+            grad_sum /= batch_size
+
+    def grad(self, x: float, d: float, y: float, v: np.ndarray) -> np.ndarray:
+        """
+        Evaluate the gradient of the MSE, key step of backpropagation algorithm
+
+        ### Input parameters
+        - x: individual training input
+        - d: individual training output
+        - y: output associated with weights w
+        - v: array of intermediate values (N x 1) associated with input x - dimensions are checked
+
+        ### Output variables
+        - grad_mse: (3N+1 x 1) vector containing the gradient of MSE wrt each element of w
+        """
+        if len(v) != self.n_hidden:
+            raise ValueError(
+                f"The length of the vector of local fields should be {self.n_hidden}."
+            )
+        v = v.reshape((self.n_hidden, 1))
+
+        b_i = self.w[: self.n_hidden]  # Biases of 1st layer
+        w_ij = self.w[self.n_hidden : 2 * self.n_hidden]  # Weights of 1st layer
+        w_prime_1j = self.w[
+            2 * self.n_hidden : 3 * self.n_hidden
+        ]  # Weights of 2nd layer (to output)
+        b_prime = self.w[-1]  # Weight of output
+
+        grad_mse = np.zeros((3 * self.n_hidden + 1, 1))
+        # NOTE: derivative of output activation function is 1 (linear)
+        for i in range(3 * self.n_hidden + 1):
+            if i in range(0, self.n_hidden):
+                # Gradient wrt biases of neurons in 1st layer
+                grad_mse[i] = -1 * (d - y) * self.phi_prime(v[i]) * w_prime_1j[i]
+            elif i in range(self.n_hidden, 2 * self.n_hidden):
+                # Gradient wrt weights of neurons in second layer
+                grad_mse[i] = (
+                    -1
+                    * x
+                    * (d - y)
+                    * self.phi_prime(v[i - self.n_hidden])
+                    * w_prime_1j[i - self.n_hidden]
+                )
+            elif i in range(2 * self.n_hidden, 3 * self.n_hidden):
+                # Gradient wrt weights of output neuron
+                grad_mse[i] = -1 * self.phi(v[i - 2 * self.n_hidden]) * (d - y)
+            else:
+                # Gradient wrt bias of output neuron
+                assert i == 3 * self.n_hidden
+                grad_mse[i] = -1 * (d - y)
+
+        return grad_mse
+
+    def forward(self, x: float) -> [float, np.ndarray]:
+        """
+        Evaluate the output of the neural network
+
+        ### Input parameters
+        - x: input of NN (single value, in this case)
+
+        ### Output values
+        - y: output of NN
+        - v: intermediate local fields at central layer (before activation)
+        """
+        # Isolate elements in array w
+        b_i = self.w[: self.n_hidden]  # Biases of 1st layer
+        w_ij = self.w[self.n_hidden : 2 * self.n_hidden]  # Weights of 1st layer
+        w_prime_ij = self.w[
+            2 * self.n_hidden : 3 * self.n_hidden
+        ]  # Weights of 2nd layer (to output)
+        b_prime = self.w[-1]  # Weight of output
+
+        # Evaluate intermediate values:
+        v = x * w_ij + b_i
+        # Pass them through activation function:
+        z = self.phi(v)
+        # Evaluate output (y)
+        y = sum(z * w_prime_ij) + b_prime
+
+        return y, v
 
 
 def grad_mse(
@@ -173,7 +406,7 @@ def backpropagation(
     y_curr = np.zeros((n, 1))
     v_curr = np.zeros((n, N))  # Row contains values for element x_i
     for i in range(n):
-        y_curr[i], v = nn_output(x[i], w, N, phi)
+        y_curr[i], v = forward(x[i], w, N, phi)
         v_curr[i, :] = v.T
 
     mse_curr = mse(d, y_curr)
@@ -213,7 +446,7 @@ def backpropagation(
         # Update weights - BP
         for i in range(n):
             # Update weights for every element in training set
-            y, v = nn_output(x[i], w, N, phi)
+            y, v = forward(x[i], w, N, phi)
             y_curr[i] = y
             grad_mse_curr = grad_mse(
                 x[i],
@@ -306,7 +539,7 @@ def main(n: int, N: int, img_folder: str, plots: bool = False):
     y_plot_est = np.zeros((1000, 1))
     for i in range(len(x_plot)):
         # Need to center the test elements
-        y_plot_est[i] = nn_output(x_plot[i] - mean_x, w_0, N, np.tanh)[0]
+        y_plot_est[i] = forward(x_plot[i] - mean_x, w_0, N, np.tanh)[0]
 
     fig, ax = plt.subplots(figsize=(8, 6), tight_layout=True)
     ax.plot(x_plot, y_plot_est, "b", label="NN output")
