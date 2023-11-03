62b0eb6d74f94a9315cc6b67546d58fee5112166
Jake Zeisel <jacobzeisel@gmail.com>
2023-11-03 12:43:48 -0500

    make client a Class, change to object approach

--- client/client.py
+++ client/client.py
@@ -6,66 +6,70 @@ import numpy as np
 import json
 import time
 
-num_rounds = 0
-localhost = 'http://localhost:5555/'
+class ClientNode:
+    PID = str(getpid())
+    server_port = ""
+    addr = "http://localhost:"
+    client_model = CurveFitter(24, targetFunction)
+    num_rounds = 0
+    current_tx = np.array([])
+    current_ty = np.array([])
+    current_grad_matrix = np.array([])
 
-local_model = CurveFitter(24, targetFunction)
-
-def request_data_thread():
-    global num_rounds
-    while(1):
-        print(f"requesting data: round {num_rounds}")
-        ## blocks until server has data for client
-        r = requests.get(localhost + 'data')
+    def __init__(self, server_port):
+        self.server_port = server_port
+        self.addr += (self.server_port + "/")
+    def register_with_server(self):
+        print(f"{self.PID}: registering with server")
+        r = requests.post(self.addr, data={'pid':self.PID})
         if r.status_code != 200:
-            print(f"uh oh. bad response from server [2]: round {num_rounds}")
-            return
-        ## get json data from server response
-        ## will contain test data
+            raise Exception(f"{self.PID}: bad response from server on client registration")
+    def request_data_from_server(self):
+        print(f"{self.PID}: requesting data - round {self.num_rounds}")
+        # blocks until we get data from server
+        r = requests.get(self.addr + f"dataset&pid={self.PID}")
+        if r.status_code != 200:
+            raise Exception(f"{self.PID}: bad response from server on request data - round {self.num_rounds}")
         try:
             data = r.json()
             t_x = np.array(data["t_x"])
             t_y = np.array(data["t_y"])
-            print(f"successfully received training data: round {num_rounds}")
+            self.current_tx = t_x
+            self.current_ty = t_y
         except Exception as e:
-            print(f"failed to parse JSON: {e}")
-            ## if json fails to parse, go to next server round and wait
-            continue
-
-        ## now train model with new data
-        w_n0 = local_model.w
-        local_model.assignTrainSet(t_x, t_y)
+            raise e
+        return t_x, t_y
+    def train_with_new_data(self, t_x, t_y):
+        current_weights = self.client_model.w
+        self.client_model.assignTrainSet(t_x, t_y)
         start_time = time.time()
-        grad_matrix_train = local_model.train(100, 5e-3, 1)
+        self.current_grad_matrix = self.client_model.train(100, 5e-3, 1)
         end_time = time.time()
-        ## send gradients back to server
-        ## create a json with the requisite information
-        w_n1 = (w_n0 - local_model.w).tolist()
         train_time = end_time - start_time
-        json_dict = {"new_weights":w_n1,"time":train_time}
+        json_dict = {"grad_matrix":self.current_grad_matrix.tolist(),"time":train_time}
         json_obj = json.dumps(json_dict)
-        r = requests.post(localhost + 'grads',json=json_obj)
+        return json_obj
+    def send_data_to_server(self, json_obj):
+        r = requests.post(self.addr + f"datacollect&pid={self.PID}",json=json_obj)
         if r.status_code != 200:
-            print(f"uh oh. bad response from server [3]: round {num_rounds}")
-            return
-        num_rounds += 1
-
+            raise Exception(f"{self.PID} bad response from server on send data - round {self.num_rounds}")
+        self.num_rounds += 1
+        return True
 
 def main():
-    pid = str(getpid())
-    print(f"starting client node-- PID {pid}")
+    server_port = input("input central server port: ").strip()
+    my_node = ClientNode(server_port)
+    my_node.register_with_server()
 
-    try:
-        r = requests.post(localhost + 'post', data={'pid':pid})
-    except:
-        print("uh oh. must start server before clients!")
-        return
-    if r.status_code != 200:
-        print("uh oh. bad response from server [1]")
-        return
-    rdt = threading.Thread(target=request_data_thread)
-    rdt.start()
-
+    while(1):
+        t_x, t_y = my_node.request_data_from_server()
+        json_obj = my_node.train_with_new_data(t_x, t_y)
+        success = my_node.send_data_to_server(json_obj)
+        if success is True:
+            continue
+        else:
+            break
+    return
 
 if __name__=="__main__":
     main()
